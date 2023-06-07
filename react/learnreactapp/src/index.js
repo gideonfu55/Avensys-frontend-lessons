701f5620bc1daff1ed06ac7cc745e59740ed1f71
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-07 22:33:26 +0800

    update index

--- react/learnreactapp/src/index.js
+++ react/learnreactapp/src/index.js
@@ -8,15 +8,14 @@
 // import Login from "./components/Login";
 // import Ecommerce from "./components/Ecommerce";
 
-// import Mobile from "./components/Mobile"
-// import Laptop from "./components/Laptop"
-// import Book from "./components/Book"
-// import GoogleLogin from "./components/GoogleLogin"
-// import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Mobile from "./components/Mobile"
+import Laptop from "./components/Laptop"
+import Book from "./components/Book"
+import GoogleLogin from "./components/GoogleLogin"
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 import { render } from "react-dom";
 import { createContext, useState } from "react";
-import WeatherWidget from "./components/weather/WeatherWidget";
 
 // How to use one component's useState in another component:
 export const myBasket = createContext();
@@ -36,35 +35,35 @@ function App() {
 
   return (
     <div className="mt-2">
+    {/* React router links */}
+    <BrowserRouter>
+      <Link className="mx-2" to="/mobile">Mobile</Link>
+      <Link className="mx-2" to="/laptop">Laptop</Link>
+      <Link className="mx-2" to="/book">Book</Link>
+
+      { isLoggedIn? <button className="btn btn-primary btn-sm" onClick={logout}>Logout</button> : <Link className="mx-2 btn btn-primary btn-sm" to="/GoogleLogin">Login</Link>}
+
+      <Routes>
+        <Route path="/mobile" element={<Mobile/>}></Route>
+        <Route path="/laptop" element={<Laptop/>}></Route>
+        <Route path="/book" element={<Book/>}></Route>
+        <Route path="/GoogleLogin" element=
+          {
+            <myBasket.Provider value={{ set: setIsLoggedIn }}>
+              <GoogleLogin/>
+            </myBasket.Provider>
+          }>
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
+
       {/* <Todo/> */}
       {/* <Login/> */}
       {/* <Ecommerce/> */}
-      <WeatherWidget/>
     </div>
   );
 }
 
 render(<App/>, document.getElementById("root"))
 
-{/* React router links */}
-{/* <BrowserRouter>
-  <Link className="mx-2" to="/mobile">Mobile</Link>
-  <Link className="mx-2" to="/laptop">Laptop</Link>
-  <Link className="mx-2" to="/book">Book</Link>
-
-  { isLoggedIn? <button className="btn btn-primary btn-sm" onClick={logout}>Logout</button> : <Link className="mx-2 btn btn-primary btn-sm" to="/GoogleLogin">Login</Link>}
-
-  <Routes>
-    <Route path="/mobile" element={<Mobile/>}></Route>
-    <Route path="/laptop" element={<Laptop/>}></Route>
-    <Route path="/book" element={<Book/>}></Route>
-    <Route path="/GoogleLogin" element=
-      {
-        <myBasket.Provider value={{ set: setIsLoggedIn }}>
-          <GoogleLogin/>
-        </myBasket.Provider>
-      }>
-    </Route>
-
-  </Routes>
-</BrowserRouter> */}
