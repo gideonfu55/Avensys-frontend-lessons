17c8b2f6418b40141b949b5ee9454822d3a8cb64
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-07 22:57:15 +0800

    update styles

--- react/weather-widget/src/index.js
+++ react/weather-widget/src/index.js
@@ -4,7 +4,7 @@ import WeatherWidget from "./WeatherWidget";
 function App() {
 
   return (
-    <div className="mt-2 d-flex">
+    <div className="mt-2 mx-4 d-flex">
       <WeatherWidget city="Singapore"/>
       <WeatherWidget city="London"/>
       <WeatherWidget city="New York"/>
