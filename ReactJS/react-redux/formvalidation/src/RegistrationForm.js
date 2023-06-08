bdd728e2d371cb54b8d66d4d6bed70ef1759cff6
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-08 15:35:26 +0800

    add Formik form values

---
+++ ReactJS/react-redux/formvalidation/src/RegistrationForm.js
@@ -0,0 +1,42 @@
+import { useFormik } from 'formik'
+import React from 'react'
+
+function RegistrationForm() {
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+      email: ''
+    },
+    onSubmit: values => {
+      console.log('Form data', values)
+    }
+  })
+
+  return (
+    <div className='mx-3'>
+    <h5>Registration Form</h5>
+      <form>
+        <label>Enter Username:</label>
+        <br />
+        <input type="text" name='username' value={formik.initialValues.username}/>
+
+        <br /><br />
+        <label>Enter Password:</label>
+        <br />
+        <input type="text" name='password' value={formik.initialValues.password}/>
+
+        <br /><br />
+        <label>Enter Email:</label>
+        <br />
+        <input type="text" name='email' value={formik.initialValues.email}/>
+
+        <br /><br />
+        <button className='btn btn-primary' type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default RegistrationForm
--- ReactJS/react-redux/formvalidation/src/index.js
+++ ReactJS/react-redux/formvalidation/src/index.js
@@ -1,10 +1,12 @@
 import { render } from "react-dom";
+import RegistrationForm from "./RegistrationForm";
 
 function App() {
   return (
     <div>
       <h5 className="m-3 fw-bold">Welcome to Form Validation</h5>
 
+      <RegistrationForm />
     </div>
   )
 }
