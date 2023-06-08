dd5b6cfe549581a0b30f1651b5d775f7a75fdb51
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-08 13:13:57 +0800

    add method to save input to Firestore db

--- ReactJS/react-redux/databaseapp/src/Data.js
+++ ReactJS/react-redux/databaseapp/src/Data.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import { db } from './authentication/firebase'
 
 function Data() {
 
@@ -9,22 +10,36 @@ function Data() {
   const collectTitle = (event) => setMyTitle(event.target.value)
   const collectDescription = (event) => setMyDescription(event.target.value)
 
-  // How to store your data fields in the database (e.g. using Firebase + Firestore)
+  // How to store your data fields in the database (e.g. using Firebase + Firestore):
+  // 1. Access the collection in Firestore.
+  // 2. Use add method to add a new document to the collection.
+  //    - Give an alert if you wish.
+  // 3. Place in a try catch block for error handling.
+
+  const saveDataToDB = () => {
+    try {
+      db.collection("myData").add({
+        "title": myTitle,
+        "description": myDescription
+      },
+      alert('Your data has been saved!')
+    )}
+    catch (error) {
+      alert(error)
+    }
+  }
 
   return (
     <div className='m-3'>
       <label className='fw-bold mt-1 mb-1'>Enter Title:</label>
       <br/>
       <input onChange={collectTitle} type='text'/>
-
       <br/>
-      
       <label className='fw-bold mt-4 mb-1'>Enter Description:</label>
       <br/>
       <textarea onChange={collectDescription} rows={10} cols={25}></textarea>
-      <br/>
-      <br/>
-      <button className='btn btn-primary'>Save</button>
+      <br/><br/>
+      <button onClick={saveDataToDB} className='btn btn-primary'>Save</button>
     </div>
   )
 }
