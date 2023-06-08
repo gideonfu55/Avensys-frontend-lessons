05ec0a3c0419b82de577f807d973c31b19d8fa22
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-08 16:49:02 +0800

    update validation criteria

--- ReactJS/react-redux/formvalidation/src/RegistrationForm.js
+++ ReactJS/react-redux/formvalidation/src/RegistrationForm.js
@@ -11,13 +11,41 @@ function RegistrationForm() {
     },
 
     validate: data => {
-      console.log(data)
+      let errors = {}
+
+      if (!data.username) {
+        errors.username = "Username is required, cannot be empty."
+      }
+
+      if (!data.username.match(/^[A-Za-z0-9]{3,}$/)) {
+        errors.username = "Username must be at least 3 characters long."
+      }
+      
+      if (!data.password) {
+        errors.password = "Password is required, cannot be empty."
+      }
+
+      if (!data.password.match(/^[A-Za-z0-9]{8,}$/)) {
+        errors.password = "Password must be at least 8 characters long."
+      }
+
+      if (!data.email) {
+        errors.email = "Email is required, cannot be empty."
+      }
+
+      if (!data.email.match(/^[A-Za-z0-9]{3,}@[A-Za-z0-9]{3,}\.[A-Za-z0-9]{2,}$/)) {
+        errors.email = "Email must be in email format"
+      }
+
+      return errors
     },
+
     onChange: values => {
-      console.log('Form data', values)
+      // console.log('Form data', values)
     },
+
     onSubmit: values => {
-      console.log('Form data', values)
+      // console.log('Form data', values)
     }
   })
 
@@ -28,16 +56,19 @@ function RegistrationForm() {
         <label>Enter Username:</label>
         <br/>
         <input type="text" name='username' value={formik.values.username} onChange={formik.handleChange} />
+        { formik.errors.username ? <span className='text-danger ms-2'>{formik.errors.username}</span> : null }
 
         <br/><br/>
         <label>Enter Password:</label>
         <br/>
         <input type="text" name='password' value={formik.values.password} onChange={formik.handleChange} />
+        { formik.errors.password ? <span className='text-danger ms-2'>{formik.errors.password}</span> : null }
 
         <br/><br/>
         <label>Enter Email:</label>
         <br/>
         <input type="text" name='email' value={formik.values.email} onChange={formik.handleChange} />
+        { formik.errors.email ? <span className='text-danger ms-2'>{formik.errors.email}</span> : null }
 
         <br/><br/>
         <button className='btn btn-primary' type="submit">Submit</button>
