5fd6c872a7de445857d967b458e08e3529b27139
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-08 10:52:27 +0800

    update

--- ReactJS/react-redux/databaseapp/src/index.js
+++ ReactJS/react-redux/databaseapp/src/index.js
@@ -3,7 +3,7 @@ import { render } from "react-dom";
 function App() {
   return (
     <div>
-      <h5 className="m-3 fw-bold">React Connection to a Dat</h5>
+      <h5 className="m-3 fw-bold">React Connection to a Database</h5>
     </div>
   )
 }
