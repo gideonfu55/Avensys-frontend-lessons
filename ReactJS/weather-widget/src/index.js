100fcd32ee6e91756c8e89ec324751d8e3387404
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-08 08:38:39 +0800

    add responsive bootstrap

--- ReactJS/weather-widget/src/index.js
+++ ReactJS/weather-widget/src/index.js
@@ -4,7 +4,7 @@ import WeatherWidget from "./WeatherWidget";
 function App() {
 
   return (
-    <div className="mt-2 mx-4 d-flex">
+    <div className="mt-2 mx-4 d-lg-flex">
       <WeatherWidget city="Singapore"/>
       <WeatherWidget city="London"/>
       <WeatherWidget city="New York"/>
