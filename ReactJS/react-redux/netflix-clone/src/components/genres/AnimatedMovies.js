0a79c6a1ce0a35079059acfbed5b302abde74266
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-09 12:32:59 +0800

    add movie-trailer and react-youtube dependencies

---
+++ ReactJS/react-redux/netflix-clone/src/components/genres/AnimatedMovies.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+import get from 'axios'
+import ".././style.css"
+
+function AnimatedMovies() {
+
+  const apiKey = 'a3f0d1b1d5874d21139169f1e0790276'
+  const [ comedyMovies, setComedyMovies ] = useState([])
+
+  useEffect(() => {
+    // Logic to import action movies from the TMDB API:
+    get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=16`)
+      .then(response => {
+        console.log(response.data.results)
+        setComedyMovies(response.data.results)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
+
+  return (
+    <div className='mt-5'>
+      <h4 className='text-light ms-3 mb-3'>Animated Movies</h4>
+      <div className='movies d-flex'>
+        {comedyMovies.map(movie => (
+          <div className='movie mx-3 mb-3' key={movie.id}>
+            <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title} />
+            <div className='movie-info text-light mt-3'>
+              <h5>{movie.title}</h5>
+              <span>{movie.vote_average}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default AnimatedMovies
--- ReactJS/react-redux/netflix-clone/src/index.js
+++ ReactJS/react-redux/netflix-clone/src/index.js
@@ -3,13 +3,15 @@ import { render } from 'react-dom'
 import Navbar from './components/Navbar'
 import ActionMovies from './components/genres/ActionMovies'
 import ComedyMovies from './components/genres/ComedyMovies'
+import AnimatedMovies from './components/genres/AnimatedMovies'
 
 function App() {
   return (
     <div>
       <Navbar />
-      <ActionMovies/>
-      <ComedyMovies/>
+      <ActionMovies />
+      <ComedyMovies />
+      <AnimatedMovies />
     </div>
   )
 }
