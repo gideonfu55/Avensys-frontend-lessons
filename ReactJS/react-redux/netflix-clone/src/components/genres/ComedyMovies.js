b6753a5b957dfa9eb66ccb354d6de6313da8c49b
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-09 11:48:18 +0800

    add and update comedy movies

---
+++ ReactJS/react-redux/netflix-clone/src/components/genres/ComedyMovies.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import get from 'axios'
+
+function ComedyMovies() {
+
+  const apiKey = 'a3f0d1b1d5874d21139169f1e0790276'
+  const [ comedyMovies, setComedyMovies ] = useState([])
+
+  useEffect(() => {
+    // Logic to import action movies from the TMDB API:
+    get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=35`)
+      .then(response => {
+        console.log(response.data.results)
+        setComedyMovies(response.data.results)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
+
+  return (
+    <div className='mt-3'>
+      <h4 className='text-light ms-3 mb-3'>Comedy Movies</h4>
+      <div className='movies d-flex overflow-auto'>
+        {comedyMovies.map(movie => (
+          <div className='movie mx-3 mb-3' key={movie.id}>
+            <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title} />
+            <div className='movie-info text-light mt-3'>
+              <h5>{movie.title}</h5>
+              <span>{movie.vote_average}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default ComedyMovies
--- ReactJS/react-redux/netflix-clone/src/index.js
+++ ReactJS/react-redux/netflix-clone/src/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { render } from 'react-dom'
 import Navbar from './components/Navbar'
 import ActionMovies from './components/genres/ActionMovies'
+import ComedyMovies from './components/genres/ComedyMovies'
 
 function App() {
   return (
     <div>
       <Navbar />
       <ActionMovies/>
+      <ComedyMovies/>
     </div>
   )
 }
