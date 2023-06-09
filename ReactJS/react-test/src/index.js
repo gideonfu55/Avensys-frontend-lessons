7a381f141396b39a60aab0ceeed161d22ebecee8
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-09 14:46:43 +0800

    add react test app

---
+++ ReactJS/react-test/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+function App() {
+  return (
+    <div>
+      <h3>React Test App</h3>
+    </div>
+  )
+}
+
+render(<App />, document.getElementById('root'))
