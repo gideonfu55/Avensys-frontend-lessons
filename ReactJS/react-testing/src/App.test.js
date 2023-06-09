29d59e3ffc0d2c20e3767ef7e0165ff9b96dc7fc
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-09 15:16:30 +0800

    add test case

--- ReactJS/react-testing/src/App.test.js
+++ ReactJS/react-testing/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', async() => {
   render(<App />);
-  const myData = await screen.findByText("Testing React code");
+  const myData = await screen.findByText("Testing React Code");
   expect(myData).toBeInTheDocument();
 });
