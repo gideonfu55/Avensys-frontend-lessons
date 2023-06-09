8e58fc004f2a2e41ec4e86499c3b8c75ab5331cf
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-09 10:01:24 +0800

    add Netflix Clone app project

---
+++ ReactJS/react-redux/netflix-clone/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <img width={130} height={70} src="https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png" alt="Netflix Logo" />
+    </div>
+    
+  )
+}
+
+export default Navbar
---
+++ ReactJS/react-redux/netflix-clone/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+function App() {
+  return (
+    <div>We are creating a Netflix clone!</div>
+  )
+}
+
+render(<App />, document.getElementById('root'))
